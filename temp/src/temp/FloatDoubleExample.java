b4980da363ee3e7311baf1d4f9ec25585ca784f3
JustArchon <puppy1900@naver.com>
2024-09-27 17:00:22 +0900

    2024-09-27: Study

---
+++ temp/src/temp/FloatDoubleExample.java
@@ -0,0 +1,22 @@
+package temp;
+
+public class FloatDoubleExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//실수 값 저장
+		double var1 = 3.14;
+		//float var2 = 3.14;
+		float var3 = 3.14F;
+		
+		// 정밀도 테스트
+		double var4 = 0.1234567890123456789;
+		float var5 = 0.1234567890123456789F;
+		
+		System.out.println("var1: " + var1);
+		System.out.println("var2: " + var3);
+		System.out.println("var4: " + var4);
+		System.out.println("var5: " + var5);
+	}
+
+}
